5d29a826ef20513eb20237579bf46a477d53c0d6
lauriehouseaux <lma.houseaux@gmail.com>
2015-11-05 20:20:14 +0100

    ajout de transposee

--- simplesMatricesFonctions.py
+++ simplesMatricesFonctions.py
@@ -1,6 +1,26 @@
+##Entree :
+##        m : n'importe quelle matrice representee comme une liste de listes
+##Sortie :
+##	pas de retour
 def afficherMat( m ) :
-	# m : n'importe quelle matrice representee comme une liste de listes
-
-	# pas de retour
 	for i in range( len( m ) ) :
 		print( m[ i ] )
+
+
+
+
+##Entree :
+##        m : matrice de taille n*p
+##Sortie :
+##        matrice transposee de m
+def transpose(m):
+    nb_lig=len(m)
+    nb_col=len(m[0])
+    aux=[]
+    for i in range(nb_col):
+        L=[]
+        for j in range(nb_lig):
+            L.append(m[j][i])
+        aux.append(L)
+    return aux
+

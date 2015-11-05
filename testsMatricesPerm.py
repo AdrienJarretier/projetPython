1950e2e12be3371d2e6b800486a594d0799ce2ba
Jarretier Adrien <jarretier.adrien@gmail.com>
2015-11-05 15:48:49 +0100

    deplacement de afficherMat dans un fichier dedie aux fonctions basiques s'appliquants aux matrices

--- testsMatricesPerm.py
+++ testsMatricesPerm.py
@@ -1,11 +1,6 @@
 from MatricesPermutation import *
+from simplesMatricesFonctions import *
 
-def afficherMat( m ) :
-	# m : n'importe quelle matrice representee comme une liste de listes
-
-	# pas de retour
-	for i in range( len( m ) ) :
-		print( m[ i ] )
 
 
 

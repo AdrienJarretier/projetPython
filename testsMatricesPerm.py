a7c5c7c7ce3355cee62b912b77353bf673a2d9fe
Jarretier Adrien <jarretier.adrien@gmail.com>
2015-11-05 15:21:15 +0100

    test de fonction MatricePermutation : erreur dans l'initialisation de la matrice

--- testsMatricesPerm.py
+++ testsMatricesPerm.py
@@ -1,5 +1,13 @@
 from MatricesPermutation import *
 
-for i in range(5) :
-	for j in range(3) :
-		print(PermutationAleatoire(i+1))
+for i in range( 5 ) :
+
+	for j in range( 3 ) :
+
+		nUplet = PermutationAleatoire( i + 1 )
+
+		print( "PermutationAleatoire", i + 1 , " : ", nUplet )
+
+		print( " MatricePermutation( ", nUplet , " ) : " )
+
+		print( MatricePermutation( nUplet ) )

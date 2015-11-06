4480f83c81244fcefee67a0a90e55c50472c78f2
adrienJarretier <jarretier.adrien@gmail.com>
2015-11-06 09:34:55 +0100

    fonction de génération de matrices nulles

--- simplesMatricesFonctions.py
+++ simplesMatricesFonctions.py
@@ -1,6 +1,29 @@
-def afficherMat( m ) :
+
+'''
+MatNulle
+
+Entree :
+	n : un entier
+	p : un entier
+
+Sortie :
+	Matrice nulle de taille (n, p) représentée par une liste de listes
+'''
+def MatNulle(n, p) :
+	m = []
+	for i in range(n) :
+		L = []
+		for j in range(p):
+			L.append(0)
+
+		m.append(L)
+
+	return m
+
+def AfficherMat( m ) :
 	# m : n'importe quelle matrice representee comme une liste de listes
 
 	# pas de retour
 	for i in range( len( m ) ) :
 		print( m[ i ] )
+

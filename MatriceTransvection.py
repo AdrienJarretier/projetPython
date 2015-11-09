83f0e044d5aecaa94f1db7505d52033690938905
lauriehouseaux <lma.houseaux@gmail.com>
2015-11-09 19:47:46 +0100

    MatriceTransvection.py

---
+++ MatriceTransvection.py
@@ -0,0 +1,21 @@
+##MatriceTransvection
+##
+##On notera une transvection : t(i,a,j)
+##
+##Matrice identite + Matrice Eij
+##
+##Entree :
+##    n : taille des matrices
+##    i : entier : indice de la ligne à remplacer : 1 <= i <=n
+##    a : reel : coefficient de multiplication de la ligne j
+##    j : entier : indice de la ligne de remplacement : 1 <= n ET j!=i
+##Sortie :
+##    Liste de listes : Matrice de transvection
+
+def MatriceTransvection (i,a,j):
+    Id=MatId(n)
+    return Id[i][j]=a
+
+
+    
+    

8454e8ecbedd7bda5b5ab53e9cf03e72f6b54b9b
adrienJarretier <jarretier.adrien@gmail.com>
2015-11-12 09:59:47 +0100

    tests de MatriceTransvection

--- testsMatricesPerm.py
+++ testsMatricesPerm.py
@@ -1,5 +1,14 @@
 from MatricesPermutation import *
+from MatriceTransvection import *
+
 from simplesMatricesFonctions import *
+
+def testMatriceTransvection( t ) :
+    print("")
+    print( "MatriceTransvection", t, " : " )
+    print( "matrice (", t[0], ",", t[0], ")" )
+    print( "coefficient :", t[2], ", ligne :",t[1], ", colonne :" ,t[3] )
+    AfficherMat( MatriceTransvection( t[0], t[1], t[2], t[3] ) )
 
 
 # A = [   [ 1, 1 ],
@@ -33,54 +42,63 @@ from simplesMatricesFonctions import *
 
 # -------------------------------------- EstPermutation --------------------------------------
 
-M=[
-    [1,0,0,0],
-    [0,0,0,1],
-    [0,1,0,0],
-    [0,0,1,1]
-    ]
+# M=[
+#     [1,0,0,0],
+#     [0,0,0,1],
+#     [0,1,0,0],
+#     [0,0,1,1]
+#     ]
 
-print(EstPermutation(M))
-
-
-P=[
-    [1,0,0,0],
-    [0,0,0,0],
-    [0,1,0,0],
-    [0,0,1,0]
-    ]
-print(EstPermutation(P))
+# print(EstPermutation(M))
 
 
-T=[
-    [1,0,0,0],
-    [0,0,0,1],
-    [0,1,0,0],
-    [0,0,0,1]
-    ]
-print(EstPermutation(T))
+# P=[
+#     [1,0,0,0],
+#     [0,0,0,0],
+#     [0,1,0,0],
+#     [0,0,1,0]
+#     ]
+# print(EstPermutation(P))
 
-R=[
-    [1,0,0,0],
-    [0,0,0,1],
-    [0,1,0,0],
-    ]
-print(EstPermutation(R))
 
-# -------------------------------------- ProduitPermutD --------------------------------------
+# T=[
+#     [1,0,0,0],
+#     [0,0,0,1],
+#     [0,1,0,0],
+#     [0,0,0,1]
+#     ]
+# print(EstPermutation(T))
 
-M=[
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
-    [13,14,15,16]
-    ]
+# R=[
+#     [1,0,0,0],
+#     [0,0,0,1],
+#     [0,1,0,0],
+#     ]
+# print(EstPermutation(R))
 
-P=[
-    [0,0,1,0],
-    [0,1,0,0],
-    [0,0,0,1],
-    [1,0,0,0]
-    ]
+# # -------------------------------------- ProduitPermutD --------------------------------------
 
-AfficherMat(ProduitPermutD(M,P))
+# M=[
+#     [1,2,3,4],
+#     [5,6,7,8],
+#     [9,10,11,12],
+#     [13,14,15,16]
+#     ]
+
+# P=[
+#     [0,0,1,0],
+#     [0,1,0,0],
+#     [0,0,0,1],
+#     [1,0,0,0]
+#     ]
+
+# AfficherMat(ProduitPermutD(M,P))
+
+t = ( 3, 1, 3, 3 )
+testMatriceTransvection( t )
+
+t = ( 5, 3, 12, 5 )
+testMatriceTransvection( t )
+
+t = ( 4, 3, 2, 1 )
+testMatriceTransvection( t )

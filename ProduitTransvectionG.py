1c8183126aae469242a95d29f63ab695e4016507
lauriehouseaux <lma.houseaux@gmail.com>
2015-11-14 21:41:08 +0100

    Modification+test ProduitTransvectionG

--- ProduitTransvectionG.py
+++ ProduitTransvectionG.py
@@ -1,3 +1,8 @@
+from copy import deepcopy
+from MatriceTransvection import *
+from simplesMatricesFonctions import *
+from testProduitTransvectionD import *
+
 ## ProduitTransvectionG
 ##
 ## Entree :
@@ -8,7 +13,25 @@
 ##	Liste de listes : matrice produit ( T * M )
     
 def ProduitTransvectionG(T,M):
-    for k in range(len(M)):
-        M[j][k]=a*M[j][k]
-    M[i]=M[j]
-    return M
+    P=deepcopy(M)
+    i,a,j=TransvectionAssociee(T)
+    for k in range(len(P[0])):
+        P[i][k]+=a*P[j][k]
+    return P
+
+M=[
+    [1,1,1],
+    [2,2,2],
+    [3,3,3],
+    [4,4,4]
+    ]
+
+
+
+a = 2
+i = 1
+j = 3
+t = MatriceTransvection(3,i,a,j)
+
+AfficherMat(ProduitTransvectionG(t,M))
+

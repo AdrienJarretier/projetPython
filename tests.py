913851181d40c48db764211e93391d2f43e09f32
adrienJarretier <jarretier.adrien@gmail.com>
2015-11-15 14:42:50 +0100

    debut de mise en place d'un menu interactif de tests

--- tests.py
+++ tests.py
@@ -1,10 +1,38 @@
 # from MatricesPermutation.testsMatricesPerm import *
-
 # from MatricesTransvection.testEstTransvection import *
 # from MatricesTransvection.testMatriceTransvectionInverse import *
 # from MatricesTransvection.testMatriceTransvection import *
 # from MatricesTransvection.testProduitTransvectionD import *
-from MatricesTransvection.testTransvectionAssociee import *
+# from MatricesTransvection.testTransvectionAssociee import *
 
+tests = [
+		"testsMatricesPerm",
+		"testEstTransvection"
+		]
 
+testsTaille = len( tests );
+
+continuerTests = True;
+
+while continuerTests :
+
+	for i in range( testsTaille ) :
+
+		print( i + 1 , "-" , tests[ i ] )
+
+	testNum = input("choix( 1-" + str(testsTaille) + ", 'q' pour quitter ) : ")
+
+	# print( "*",testNum,"*" )
+
+	testNum = testNum.lower()
+
+	if testNum == "q" or testNum == "quit" or testNum == "exit" :
+		continuerTests = False
+
+	if testNum == "1" :
+		# from MatricesPermutation.testsMatricesPerm import *
+		import MatricesPermutation.testsMatricesPerm
+	if testNum == "2" :
+		# from MatricesTransvection.testEstTransvection import *
+		import MatricesTransvection.testEstTransvection
 
